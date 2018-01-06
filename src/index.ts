98d97d1d67a4733f4d3dc737c4a1bc82bea54818
AFOC-1709091830\echou <echoulen@gmail.com>
2018-01-06 11:34:33 +0800

    init demo

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import {PullToRefresh} from "./components/PullToRefresh";
-import {RefreshContent} from "./components/RefreshContent";
-import {PullDownContent} from "./components/PullDownContent";
-import {ReleaseContent} from "./components/ReleaseContent";
-
-export {PullToRefresh, RefreshContent, PullDownContent, ReleaseContent};
